a2650952e1df4a7c8f8223144ea08da6533484f1
RobertGoldschmidtNXT <r.goldschmidt@nextech.com>
2019-04-01 16:37:41 -0400

    added classes for roles and players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { role } from './models/role';
+import { player } from './models/player';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,71 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TheSprint';
+  roles: role[] = 
+    [{
+      "Id": 1,
+      "Role": "The Duke",
+      "Team": "OD"
+    },
+    {
+      "Id": 2,
+      "Role": "Level III Dev",
+      "Team": "OD"
+    },
+    {
+      "Id": 3,
+      "Role": "IntelleWater",
+      "Team": "OD"
+    },
+    {
+      "Id": 4,
+      "Role": "Remote Dev",
+      "Team": "OD"
+    },
+    {
+      "Id": 5,
+      "Role": "Sniper",
+      "Team": "OS"
+    },
+    {
+      "Id": 6,
+      "Role": "Bad Billy",
+      "Team": "OS"
+    },
+    {
+      "Id": 7,
+      "Role": "Level II Dev",
+      "Team": "OS"
+    }];
+  players: player[] = 
+    [
+      {
+        "PlayerName": "John",
+        "RoleId": null
+       },
+      {
+        "PlayerName": "Dave",
+        "RoleId": null
+       },
+      {
+        "PlayerName": "Mike",
+        "RoleId": null
+       },
+      {
+        "PlayerName": "Derrick",
+        "RoleId": null
+       },
+      {
+        "PlayerName": "Kevin",
+        "RoleId": null
+       },
+      {
+        "PlayerName": "Brian",
+        "RoleId": null
+       },
+      {
+        "PlayerName": "Nick",
+        "RoleId": null
+       }
+    ]
 }
---
+++ src/app/models/player.ts
@@ -0,0 +1,6 @@
+export class player{
+    constructor(
+        public PlayerName: string,
+        public RoleId: number
+    ) {}
+}
