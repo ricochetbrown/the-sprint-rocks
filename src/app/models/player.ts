23c6cc450ea1683629930af1f1ded7a4a02f52e6
nick.yzzi <nick.yzzi@mdintellesys.com>
2019-04-01 16:56:19 -0400

    Allow players to accept or reject the team

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -1,6 +1,16 @@
 export class player{
+    Accept: boolean;
+
     constructor(
         public PlayerName: string,
         public RoleId: number
     ) {}
+
+    acceptTeam() {
+        this.Accept = true;
+    }
+
+    rejectTeam() {
+        this.Accept = false;
+    }
 }
