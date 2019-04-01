fc5469177b8edc998266f3abf9e4e726bc43312b
ricochetbrown <ricochetbrown@gmail.com>
2019-04-01 16:28:00 -0400

    Getting html setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
